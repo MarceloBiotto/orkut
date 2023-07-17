43f008d63e0fc559154b1974622e2ae80578b7d3
Marcelo Bioto <lee.am@live.com>
2023-07-17 16:36:45 -0300

    adicionando cores e upando as imagens na pasta

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function capturarInput() {
   
     
     if (person.name === "ricardo") {
-      document.getElementById("imagem").innerHTML = '<img src="ricardo.png" alt="Foto de Ricardo">';
+      document.getElementById("imagem").innerHTML = '<img src="./imagens/ricardo.png" alt="Foto de Ricardo">';
     } else if (person.name === "adao") {
-        document.getElementById("imagem").innerHTML = '<img src="adao.png.jpg" alt="Foto de adao">';
+        document.getElementById("imagem").innerHTML = '<img src="./imagens/adao.png.jpg" alt="Foto de adao">';
     }else if(person.name === "alessandra"){
-        document.getElementById("imagem").innerHTML = '<img src="alessandra.png" alt="Foto da ally">';
+        document.getElementById("imagem").innerHTML = '<img src="./imagens/alessandra.png" alt="Foto da ally">';
     }
     else if (person.name === "geovane") {
-        document.getElementById("imagem").innerHTML = '<img src="geovane.png" alt="Foto de geovane">';
+        document.getElementById("imagem").innerHTML = '<img src="./imagens/geovane.png" alt="Foto de geovane">';
     }else {
       
       document.getElementById("imagem").innerHTML = '<p>Não foi encontrada uma imagem para o nome introduzido.</p>';
